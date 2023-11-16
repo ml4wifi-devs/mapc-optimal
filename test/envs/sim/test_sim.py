f9ee3ecff1b7298c394fb3299979e9b69bb21d56
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-11-16 15:45:50 +0100

    Add tests and update scenarios

--- test/envs/sim/test_sim.py
+++ test/envs/sim/test_sim.py
@@ -4,8 +4,7 @@ import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
-from ml4wifi.envs.scenarios.static import DEFAULT_SIGMA, DEFAULT_MCS
-from ml4wifi.envs.sim import DEFAULT_TX_POWER, network_throughput
+from ml4wifi.envs.sim import DEFAULT_TX_POWER, DEFAULT_SIGMA, network_throughput
 
 
 class SimTestCase(unittest.TestCase):
@@ -53,8 +52,8 @@ class SimTestCase(unittest.TestCase):
             [0, 1, 0, 0, 0, 0]
         ])
 
-        # Modulation and coding scheme of the nodes (here, all nodes use default MCS)
-        mcs = jnp.ones(n_nodes, dtype=jnp.int32) * DEFAULT_MCS
+        # Modulation and coding scheme of the nodes (here, all nodes use MCS 4)
+        mcs = jnp.ones(n_nodes, dtype=jnp.int32) * 4
 
         # Transmission power of the nodes (all nodes use the default transmission power)
         tx_power = jnp.ones(n_nodes) * DEFAULT_TX_POWER
@@ -76,12 +75,10 @@ class SimTestCase(unittest.TestCase):
 
         # Plot the positions of the nodes
         plt.figure(figsize=(7.5, 4.5))
-        plt.rcParams['text.usetex'] = False
-
         plt.scatter(pos[:, 0], pos[:, 1], marker='x')
 
         for p, name in zip(pos, ['AP A', 'AP B', 'STA 1', 'STA 2', 'STA 3', 'STA 4']):
-            plt.text(p[0] - 2, p[1] + 1, name)
+            plt.text(p[0] - 1, p[1] + 1, name)
 
         plt.xlim(0, 50)
         plt.ylim(0, 30)
