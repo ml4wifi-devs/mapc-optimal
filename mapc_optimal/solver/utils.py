ccb97916ef34f851fe926ceabaee75fffc6e57d7
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-01-09 22:16:52 +0100

    Upgrade dependencies

--- mapc_optimal/solver/utils.py
+++ mapc_optimal/solver/utils.py
@@ -1,11 +1,8 @@
 import jax
 import jax.numpy as jnp
-import tensorflow_probability.substrates.jax as tfp
 from chex import Array, Numeric
 from mapc_sim.constants import REFERENCE_DISTANCE
 from mapc_sim.utils import tgax_path_loss as path_loss
-
-tfd = tfp.distributions
 
 
 def dbm_to_lin(x: Numeric) -> Numeric:
